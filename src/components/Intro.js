f2ce2c73799c820fc0880187a36433e41de6395e
Thomas Bouvier <tombvr75@gmail.com>
2021-11-25 15:33:04 +0100

    Intro component > basic styling

---
+++ src/components/Intro.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Intro.scss';
+
+export const Intro = () => {
+    return (
+        <div id="intro__hero">
+            <h1>
+                The agency for what comes next.
+            </h1>
+        </div>
+    )
+}
+
+export default Intro;
