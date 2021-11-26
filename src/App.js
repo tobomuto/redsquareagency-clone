7fd8f428b9c3559237c7ef3c5745f1a76e204aa7
Thomas Bouvier <tombvr75@gmail.com>
2021-11-26 13:31:24 +0100

    import components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.scss';
 
+import Header from './components/Header';
+import Intro from './components/Intro';
+import Preview from './components/Preview';
+import IdeaSection from './components/IdeaSection';
+import ClientsSection from './components/ClientsSection';
+import Footer from './components/Footer'
+
 const App = () => {
   return (
     <div className="App">
-      
+      <Header />
+      <Intro />
+      <Preview />
+      <IdeaSection />
+      <ClientsSection />
+      <Footer />
     </div>
   );
 }
