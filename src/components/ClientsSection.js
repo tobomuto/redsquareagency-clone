8fc3508491a190a16ee52a1f62a8bce244b1d3c6
Thomas Bouvier <tombvr75@gmail.com>
2021-11-26 15:53:18 +0100

    responsive clientsList

--- src/components/ClientsSection.js
+++ src/components/ClientsSection.js
@@ -7,17 +7,21 @@ import clientsJson from '../assets/datas/clientsList.json'
 const ClientsSection = () => {
     return (
         <section id="clients-section">
-            <h3>Select Clients</h3>
-            <ul> 
-                {clientsJson.filter(client => client.group === 1).map(client => 
-                    <li key={client.id}>{client.name}</li>
-                )}
-            </ul>
-            <ul> 
-                {clientsJson.filter(client => client.group === 2).map(client => 
-                    <li key={client.id}>{client.name}</li>
-                )}
-            </ul>
+            <div className="clients-section__container">
+                <h3>Select Clients</h3>
+                <div className="clients-section__lists">
+                    <ul> 
+                        {clientsJson.filter(client => client.group === 1).map(client => 
+                            <li key={client.id}>{client.name}</li>
+                        )}
+                    </ul>
+                    <ul> 
+                        {clientsJson.filter(client => client.group === 2).map(client => 
+                            <li key={client.id}>{client.name}</li>
+                        )}
+                    </ul>
+                </div>
+            </div>
             <div className="divider"></div>
         </section>
     )
