4b016c6d7a3506e072976b6fabfe143f8e898190
Thomas Bouvier <tombvr75@gmail.com>
2021-11-26 11:19:45 +0100

    IdeaSection component

---
+++ src/components/IdeaSection.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './IdeaSection.scss'
+
+const IdeaSection = () => {
+    return (
+        <section id="idea-section">
+            <h2 className="idea-section__title">
+                We deal in ideas, design and media that are category defying. When the world Millis, you’ve got to Vanilli.
+            </h2>
+            <p class="idea-section__paragraph">
+                Red Square believes in rigorous development of brand strategy and whip-smart execution. We make all sorts of things. Things that move markets, compel audiences and sell product. We help great brands create what comes next.
+            </p>
+        </section>
+    )
+}
+
+export default IdeaSection
