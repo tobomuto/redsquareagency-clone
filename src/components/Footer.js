9fe88908785f7128e2027f2e86bfc0fdf8770f2b
Thomas Bouvier <tombvr75@gmail.com>
2021-11-26 13:32:03 +0100

    footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {RiInstagramFill} from 'react-icons/ri';
+import {RiTwitterFill} from 'react-icons/ri';
+import {RiLinkedinBoxFill} from 'react-icons/ri';
+import imageOne from '../assets/works-preview/1.jpg'
+// import Nugs from '../assets/nugs.svg';
+import './Footer.scss';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer__infos">
+                <div className="footer__email">
+                    <a href="mailto:hi@rsq.com" class="panel">
+                        <h4>Let’s talk.</h4> 
+                        <span>hi@rsq.com</span>
+                    </a>
+                </div>
+                <div className="stack">
+                    <ul className="footer__social">
+
+                        <li><RiInstagramFill/></li>
+                        <li><RiTwitterFill/></li>
+                        <li><RiLinkedinBoxFill/></li>
+                    </ul>
+                    <ul className="footer__cities">
+                        <li>Mobile, AL</li>
+                        <li>Chicago, IL</li>
+                        <li>Tulsa, OK</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="img-cont">
+                <img src={imageOne} alt="blured"/>
+                <p>Work</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
