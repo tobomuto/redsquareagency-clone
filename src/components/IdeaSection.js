da95aaf7cdf99d14ee555006033e26aba837cf55
Thomas Bouvier <tombvr75@gmail.com>
2021-11-27 12:46:18 +0100

    Idea Section responsive

--- src/components/IdeaSection.js
+++ src/components/IdeaSection.js
@@ -7,7 +7,7 @@ const IdeaSection = () => {
             <h2 className="idea-section__title">
                 We deal in ideas, design and media that are category defying. When the world Millis, you’ve got to Vanilli.
             </h2>
-            <p class="idea-section__paragraph">
+            <p className="idea-section__paragraph">
                 Red Square believes in rigorous development of brand strategy and whip-smart execution. We make all sorts of things. Things that move markets, compel audiences and sell product. We help great brands create what comes next.
             </p>
             <div className="divider"></div>
