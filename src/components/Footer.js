640366794604c82505d29cc897782b69cef79420
Thomas Bouvier <tombvr75@gmail.com>
2021-11-29 14:55:07 +0100

    animation footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import { React, useRef, useEffect } from 'react';
+
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 import {RiInstagramFill} from 'react-icons/ri';
 import {RiTwitterFill} from 'react-icons/ri';
 import {RiLinkedinBoxFill} from 'react-icons/ri';
 import imageOne from '../assets/works-preview/1.jpg'
-// import Nugs from '../assets/nugs.svg';
+
 import './Footer.scss';
 
+gsap.registerPlugin(ScrollTrigger);
+
 const Footer = () => {
+
+    const footerInfosRef = useRef();
+    const imgContRef = useRef();
+
+    useEffect(() => {
+        gsap.from(footerInfosRef.current, {
+            y: 200,
+            duration: 0.8,
+            opacity: 0,
+            ease: "power2.out",
+            scrollTrigger: {
+                trigger: ".footer__infos"
+            }
+        })
+        gsap.from(imgContRef.current, {
+            duration: 0.8,
+            opacity: 0,
+            ease: "power2.out",
+            scrollTrigger: {
+                trigger: ".img-cont"
+            }
+        })
+    });
+
     return (
         <footer>
-            <div className="footer__infos">
+            <div className="footer__infos" ref={footerInfosRef}>
                 <div className="footer__email">
                     <a href="mailto:hi@rsq.com" className="panel">
                         <h4>Let’s talk.</h4> 
@@ -30,7 +60,7 @@ const Footer = () => {
                     </ul>
                 </div>
             </div>
-            <div className="img-cont">
+            <div className="img-cont" ref={imgContRef}>
                 <img src={imageOne} alt="blured"/>
                 <p className="img-cont__work">Work</p>
                 <p className="img-cont__all-projects">All Projects</p>
