d246cf2e48d2b325a81bd502e648fcf7cbb3024e
Thomas Bouvier <tombvr75@gmail.com>
2021-11-29 14:01:01 +0100

    animation moving Preview + hover

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import { React, useRef, useEffect } from 'react';
+import { gsap } from "gsap";
 import imageOne from '../assets/works-preview/1.jpg'
 import imageTwo from '../assets/works-preview/2.jpg'
 import imageThree from '../assets/works-preview/3.jpg'
@@ -13,7 +14,11 @@ import './Preview.scss';
 
 const Preview = () => {
 
+    const imgRef = useRef();
+
     useEffect(() => {
+
+        gsap.from(imgRef.current, {delay: 3.6, transform: 'translate3d(0,20vh,0)', ease: "power1.out", duration: 1, opacity: 0});
 
         let itemClass   = 'image';
         let activeClass = 'active';
@@ -46,16 +51,18 @@ const Preview = () => {
     
     return (
         <a href="/">
-            <div className="img-container">
-                <img className="image active" src={imageOne} alt="work 1"/>
-                <img className ="image" src={imageTwo} alt="work 2"/>
-                <img className ="image" src={imageThree} alt="work 3"/>
-                <img className ="image" src={imageFour} alt="work 4"/>
-                <img className ="image" src={imageFive} alt="work 5"/>
-                <img className ="image" src={imageSix} alt="work 6"/>
-                <img className ="image" src={imageSeven} alt="work 7"/>
-                <img className ="image" src={imageHeight} alt="work 8"/>
-                <img className ="image" src={imageNine} alt="work 9"/>
+            <div className="img-container" ref={imgRef}>
+                <div className="transform-container">
+                    <img className="image active" src={imageOne} alt="work 1"/>
+                    <img className ="image" src={imageTwo} alt="work 2"/>
+                    <img className ="image" src={imageThree} alt="work 3"/>
+                    <img className ="image" src={imageFour} alt="work 4"/>
+                    <img className ="image" src={imageFive} alt="work 5"/>
+                    <img className ="image" src={imageSix} alt="work 6"/>
+                    <img className ="image" src={imageSeven} alt="work 7"/>
+                    <img className ="image" src={imageHeight} alt="work 8"/>
+                    <img className ="image" src={imageNine} alt="work 9"/>
+                </div>
             </div>
         </a>
     )
