84169e9289b207290f2f73a9028b8e14b2e0f020
Thomas Bouvier <tombvr75@gmail.com>
2021-11-29 13:00:37 +0100

    intro animation

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import { React, useRef, useEffect } from 'react';
+
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import './Intro.scss';
 
+gsap.registerPlugin(ScrollTrigger);
+
 export const Intro = () => {
+
+    const sentenceRef = useRef();
+    const q = gsap.utils.selector(sentenceRef);
+
+    useEffect(() => {
+        gsap.from(q(".word"), {delay: 1.9, opacity: 0, duration: 0.5, stagger: 0.2, ease: "power1.out"})
+    });
+
     return (
         <div id="intro__hero">
-            <h1>
-                The agency for<br/> what comes next.
+            <h1 ref={sentenceRef}>
+            <span className="word">The </span><span  className="word">agency</span> <br/> <span  className="word">what</span> <span  className="word">comes</span> <span  className="word">next.</span>
             </h1>
         </div>
     )
