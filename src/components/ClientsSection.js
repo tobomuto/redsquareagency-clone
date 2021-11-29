e5195c3587c8a0c2975fee109d6f3add8973b04c
Thomas Bouvier <tombvr75@gmail.com>
2021-11-29 14:49:06 +0100

    animation clientsSection

--- src/components/ClientsSection.js
+++ src/components/ClientsSection.js
@@ -1,15 +1,44 @@
-import React from 'react'
+import { React, useRef, useEffect } from 'react';
+
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import './ClientsSection.scss'
 
 import clientsJson from '../assets/datas/clientsList.json'
 
+gsap.registerPlugin(ScrollTrigger);
+
 const ClientsSection = () => {
+
+    const clientsListsRef = useRef();
+    const dividerRef = useRef();
+
+    useEffect(() => {
+        gsap.from(clientsListsRef.current, {
+            x: -200,
+            duration: 0.8,
+            opacity: 0,
+            ease: "power2.out",
+            scrollTrigger: {
+                trigger: ".clients-section__lists"
+            }
+        })
+        gsap.from(dividerRef.current, {
+            scaleX:0, transformOrigin:'left center',
+            duration: 2,
+            ease: "power2.out",
+            scrollTrigger: {
+                trigger: dividerRef.current
+            }
+        })
+    });
+
     return (
         <section id="clients-section">
             <div className="clients-section__container">
                 <h3>Select Clients</h3>
-                <div className="clients-section__lists">
+                <div className="clients-section__lists" ref={clientsListsRef}>
                     <ul> 
                         {clientsJson.filter(client => client.group === 1).map(client => 
                             <li key={client.id}>{client.name}</li>
@@ -22,7 +51,7 @@ const ClientsSection = () => {
                     </ul>
                 </div>
             </div>
-            <div className="divider"></div>
+            <div className="divider" ref={dividerRef}></div>
         </section>
     )
 }
